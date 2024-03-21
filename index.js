068aaa606878d52440b755d5422103ec588562ce
Muhammad Soban Saleem <sobansaleem713@gmail.com>
2024-03-22 01:20:00 +0500

    pushed simple-calculator on github

---
+++ index.js
@@ -0,0 +1,32 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+const answer = await inquirer.prompt([
+    {
+        message: "Enter Your First Number: ",
+        type: "number",
+        name: "FirstNumber",
+    },
+    {
+        message: "Enter Your Second Number: ",
+        type: "number",
+        name: "SecondNumber",
+    },
+    {
+        message: "Enter Your Operator: ",
+        type: "list",
+        name: "Operator",
+        choices: ["+", "-", "*", "/"],
+    },
+]);
+if (answer.Operator === "+") {
+    console.log("Result: ", answer.FirstNumber + answer.SecondNumber);
+}
+else if (answer.Operator === "-") {
+    console.log("Result: ", answer.FirstNumber - answer.SecondNumber);
+}
+else if (answer.Operator === "*") {
+    console.log("Result: ", answer.FirstNumber * answer.SecondNumber);
+}
+else if (answer.Operator === "/") {
+    console.log("Result: ", answer.FirstNumber / answer.SecondNumber);
+}
